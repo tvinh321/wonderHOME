a8abae306c49c21300f8e5064cdbf53f1f42b80f
wendy02062001 <wendy.nguyen262001@gmail.com>
2022-10-21 09:22:28 +0700

    feat: add material for wonderHome

---
+++ public/settings.php
@@ -0,0 +1,26 @@
+<?php
+    // This is where all the universal settings is stored
+    $ABS_ROOT_PATH = "wonderHOME";
+
+    //available pages
+    $PAGES = ["home"];
+    $SERVICES = ["mua-nha", "ban-nha", "tim-chuyen-gia"];
+    $PROPERTY = ["thong-tin"];
+
+    $NAVLINK_VIE = array(["Mua nhà", "dich-vu/".$SERVICES[0]], ["Bán nhà", "dich-vu/".$SERVICES[1]], ["Tìm chuyên gia", "dich-vu/".$SERVICES[2]], ["Hướng dẫn", ""]); 
+
+    //Font
+    $FONT_FAMILY = "Poetsen One";
+
+    //color palette
+    $PRIMARY_BLACK = "171717";
+    $PRIMARY_AMBER = "fbbf24";
+    $PRIMARY_WHITE = "fff";
+?>
+
+<style>
+html *
+{
+   font-family: <?php echo $FONT_FAMILY; ?>
+}
+</style>
