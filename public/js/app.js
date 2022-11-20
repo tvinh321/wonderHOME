5ae4803bd8e8e6a825b6ca7714099c0c86704452
wendy02062001 <wendy.nguyen262001@gmail.com>
2022-11-20 08:00:55 +0700

    fix: style

--- public/js/app.js
+++ public/js/app.js
@@ -6333,15 +6333,15 @@ function OwnerContact() {
           })
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(react_accessible_accordion__WEBPACK_IMPORTED_MODULE_1__.AccordionItemPanel, {
           children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
-            className: "flex items-center justify-center gap-x-4",
+            className: "flex items-center justify-center gap-x-10 my-10",
             children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("svg", {
               xmlns: "http://www.w3.org/2000/svg",
               viewBox: "0 0 256 256",
-              width: "16",
-              height: "16",
+              width: "20",
+              height: "20",
               children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("rect", {
-                width: "16",
-                height: "16",
+                width: "20",
+                height: "20",
                 fill: "none"
               }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("path", {
                 d: "M222,158.4l-46.9-20a15.6,15.6,0,0,0-15.1,1.3l-25.1,16.7a76.5,76.5,0,0,1-35.2-35h0L116.3,96a15.9,15.9,0,0,0,1.4-15.1L97.6,34a16.3,16.3,0,0,0-16.7-9.6A56.2,56.2,0,0,0,32,80c0,79.4,64.6,144,144,144a56.2,56.2,0,0,0,55.6-48.9A16.3,16.3,0,0,0,222,158.4Z"
@@ -6352,8 +6352,8 @@ function OwnerContact() {
               })]
             }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("svg", {
               xmlns: "http://www.w3.org/2000/svg",
-              width: "16",
-              height: "16",
+              width: "20",
+              height: "20",
               fill: "currentColor",
               "class": "bi bi-messenger",
               viewBox: "0 0 16 16",
@@ -6364,8 +6364,8 @@ function OwnerContact() {
               role: "img",
               viewBox: "0 0 24 24",
               xmlns: "http://www.w3.org/2000/svg",
-              width: "16",
-              height: "16",
+              width: "20",
+              height: "20",
               children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("title", {
                 children: "Zalo"
               }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("path", {
